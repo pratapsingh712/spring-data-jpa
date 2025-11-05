4a2b2532503928118313f979c9486500f252328d
Raghvendra Pratap Singh <rpsingh09mar@gmail.com>
2025-11-05 19:58:27 +0530

    added application context and student objects

--- src/main/java/com/raghav/spring_data_jpa_ex/SpringDataJpaExApplication.java
+++ src/main/java/com/raghav/spring_data_jpa_ex/SpringDataJpaExApplication.java
@@ -13,24 +13,24 @@ public class SpringDataJpaExApplication {
 	public static void main(String[] args) {
 		ApplicationContext context =  SpringApplication.run(SpringDataJpaExApplication.class, args);
 
-//
-//		StudentRepo repo = context.getBean(StudentRepo.class);
-//
-//		Student s1 = context.getBean(Student.class);
-//		Student s2 = context.getBean(Student.class);
-//		Student s3 = context.getBean(Student.class);
-//
-//		s1.setRollNo(101);
-//		s1.setName("Navin");
-//		s1.setMarks(75);
-//
-//		s2.setRollNo(102);
-//		s2.setName("Kiran");
-//		s2.setMarks(80);
-//
-//		s3.setRollNo(103);
-//		s3.setName("Harsh");
-//		s3.setMarks(70);
+
+		StudentRepo repo = context.getBean(StudentRepo.class);
+
+		Student s1 = context.getBean(Student.class);
+		Student s2 = context.getBean(Student.class);
+		Student s3 = context.getBean(Student.class);
+
+		s1.setRollNo(101);
+		s1.setName("Navin");
+		s1.setMarks(75);
+
+		s2.setRollNo(102);
+		s2.setName("Kiran");
+		s2.setMarks(80);
+
+		s3.setRollNo(103);
+		s3.setName("Harsh");
+		s3.setMarks(70);
 
 //		repo.save(s1);
 //		repo.save(s2);
